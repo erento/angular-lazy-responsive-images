a9426df5a5101d7bee570b1e2e44f7288d36a714
Grzegorz Janik <grzesiekjanik@hotmail.com>
2017-09-13 00:33:37 +0200

    scaffolding

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,17 @@
 import typescript from 'rollup-plugin-typescript';
-import angular from 'rollup-plugin-angular';
-import sass from 'node-sass';
-import CleanCSS from 'clean-css';
-import { minify as minifyHtml } from 'html-minifier';
 
-const pkg = require('./package.json');
-
-const cssmin = new CleanCSS();
-const htmlminOpts = {
-  caseSensitive: true,
-  collapseWhitespace: true,
-  removeComments: true,
-};
+let pkg = require('./package.json');
 
 export default {
-  input: 'src/index.ts',
-  output: {
-    file: pkg.main,
-    format: 'umd'
-  },
-  name: 'responsive.lazy.loaded.images',
+  entry: 'src/image.component.ts',
+  dest: pkg.main,
+  format: 'umd',
+  moduleName: 'responsive.lazy.loaded.images',
   external: [
-    '@angular/core',
-    '@angular/common',
+      '@angular/core',
   ],
   globals: {
     '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
   },
-  plugins: [
-  angular({
-    preprocessors: {
-      template: template => minifyHtml(template, htmlminOpts),
-      style: scss => {
-        const css = sass.renderSync({ data: scss }).css;
-        return cssmin.minify(css).styles;
-      },
-    }
-  }),
-  typescript({typescript: require('typescript')}),
-  ],
+  plugins: [typescript({typescript: require('typescript')})]
 }
