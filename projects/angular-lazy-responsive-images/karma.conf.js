7d78bbe72a2de2627c86802af387c923737138ae
Konrad Cerny <info@konradcerny.cz>
2020-07-01 23:52:27 +0200

    upgrade main project to ng10

--- projects/angular-lazy-responsive-images/karma.conf.js
+++ projects/angular-lazy-responsive-images/karma.conf.js
@@ -6,10 +6,10 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-spec-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-spec-reporter',
+      '@angular-devkit/build-angular/plugins/karma',
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
